80b475a2d245b716b6d67a1b05c50e4f6eb40f47
Buzzel <yogi.bear.development@gmail.com>
2016-03-25 20:46:13 -0700

    popular: Sort by seeds

--- PopcornTime/Popular.swift
+++ PopcornTime/Popular.swift
@@ -14,7 +14,7 @@ struct Popular: TabItem {
     let title = "Popular"
     
     func handler() {
-        NetworkManager.sharedManager().fetchMovies(limit: 50, page: 1, quality: "1080p", minimumRating: 3, queryTerm: nil, genre: nil, sortBy: "download_count", orderBy: "desc") { movies, error in
+        NetworkManager.sharedManager().fetchMovies(limit: 50, page: 1, quality: "1080p", minimumRating: 3, queryTerm: nil, genre: nil, sortBy: "seeds", orderBy: "desc") { movies, error in
             if let movies = movies {
                 let recipe = CatalogRecipe(title: "Popular Movies", movies: movies)
                 Kitchen.serve(recipe: recipe)
